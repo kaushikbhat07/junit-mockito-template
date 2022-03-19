5b6e3853f643627d2055557da33fa723b8f966f4
kaushikbhat07 <kaushik.bhat11@learner.manipal.edu>
2022-03-20 01:24:16 +0530

    Add Appointment test

--- src/test/java/patientintake/ClinicCalendarTest.java
+++ src/test/java/patientintake/ClinicCalendarTest.java
@@ -2,6 +2,10 @@ package patientintake;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +17,20 @@ class ClinicCalendarTest {
 
 	@AfterEach
 	void tearDown() {
+	}
+
+	@Test
+	void allowEntryOfAnAppointment() {
+		ClinicCalendar clinicCalendar = new ClinicCalendar();
+		clinicCalendar.addAppointment("Kaushik", "Bhat", "avery", "09/01/2018 02:00 pm");
+
+		List<PatientAppointment> appointmentList = clinicCalendar.getAppointments();
+		assertNotNull(appointmentList);
+		assertEquals(1, appointmentList.size());
+		PatientAppointment patientAppointment = appointmentList.get(0);
+		assertEquals("Kaushik", patientAppointment.getPatientFirstName());
+		assertEquals("Bhat", patientAppointment.getPatientLastName());
+		assertEquals(Doctor.avery, patientAppointment.getDoctor());
+		assertEquals("09/01/2018 02:00 PM", patientAppointment.getAppointmentDateTime().format(DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a")));
 	}
 }
