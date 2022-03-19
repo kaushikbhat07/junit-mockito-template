ebb633dd21980874aa3f636a4a7ceb98df9ee795
kaushikbhat07 <kaushik.bhat11@learner.manipal.edu>
2022-03-19 15:14:17 +0530

    Add Patient Appointment functionality

---
+++ src/test/java/patientintake/ClinicCalendarTest.java
@@ -0,0 +1,17 @@
+package patientintake;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClinicCalendarTest {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+}
